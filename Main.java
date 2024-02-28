a9f73cdc62d2570e255e45494f229d57464c8822
ThangTQ <thang.to@golfzon.com>
2024-02-28 15:30:22 +0700

    ..

--- Main.java
+++ Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        return 3;
+    }
 }
