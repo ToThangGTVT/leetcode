c62dc68f346423955343f7e5fa5585e5709378b6
ThangTQ <thang.to@golfzon.com>
2024-02-28 17:27:51 +0700

    ..

--- Main.java
+++ Main.java
@@ -1,12 +1,32 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        var solution = new Solution();
-        System.out.println(solution.lengthOfLongestSubstring("xxx"));
+        Solution solution = new Solution();
+        System.out.println("START");
+        System.out.println(solution.lengthOfLongestSubstring("zdvdfr"));
     }
 }
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        HashMap<Integer, String> hashtable = new HashMap<>();
+        hashtable.put(0, "");
+        int index = 0;
+        for (Character character: s.toCharArray()) {
+            if (hashtable.containsValue(String.valueOf(character))) {
+                index++;
+            }
+            hashtable.put(index, hashtable.get(index) + character);
+            if (hashtable.size() > max) {
+                max = hashtable.size();
+            }
 
+            System.out.println(hashtable);
+            System.out.println("============================");
+        }
+
+        return max;
     }
 }
